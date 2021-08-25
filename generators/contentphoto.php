2034f6e1b2f9f6d6f5f14d00b2b29b1c98704c46
Andrew Rivett <veggiefrog@gmail.com>
2021-08-25 11:37:05 -0400

    Updated

--- generators/contentphoto.php
+++ generators/contentphoto.php
@@ -141,7 +141,9 @@ function ciniki_wng_generators_contentphoto(&$ciniki, $tnid, $request, $block) {
                     return array('stat'=>'fail', 'err'=>array('code'=>'ciniki.wng.110', 'msg'=>'', 'err'=>$rc['err']));
                 }
                 if( isset($rc['url']) && $rc['url'] != '' ) {
-                    $buttons .= "<a class='"
+                    $buttons .= "<a "
+                        . (isset($block["button-{$i}-target"]) ? " target='" . $block["button-{$i}-target"] . "' " : '')
+                        . "class='"
                         . (isset($block['button-class']) && $block['button-class'] != '' ? $block['button-class'] : 'button')
                         . "' href='" . $rc['url'] . "'>" . $block["button-{$i}-text"] . "</a>";
                 }
