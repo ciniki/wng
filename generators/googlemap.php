28edb5ab26ff71599518e31af902e9f07cfa6d3f
Andrew Rivett <veggiefrog@gmail.com>
2023-03-22 11:39:44 -0400

    Updated calendar generator

--- generators/googlemap.php
+++ generators/googlemap.php
@@ -37,7 +37,10 @@ function ciniki_wng_generators_googlemap(&$ciniki, $tnid, $request, $block) {
 
         $js = ''
             . "function gmap_initialize{$block['sid']}() {"
-                . 'var myLatlng = new google.maps.LatLng(' . $block['latitude'] . ',' . $block['longitude'] . ');'
+                . "gmap_show('{$block['id']}',{$block['latitude']},{$block['longitude']});"
+            . "};"
+            . "function gmap_show(id,lat,long){"
+                . 'var myLatlng = new google.maps.LatLng(lat,long);'
                 . 'var mapOptions = {'
                     . 'zoom: ' . $zoom . ','
                     . 'center: myLatlng,'
@@ -46,7 +49,7 @@ function ciniki_wng_generators_googlemap(&$ciniki, $tnid, $request, $block) {
                     . 'scaleControl: true,'
                     . 'mapTypeId: google.maps.MapTypeId.ROADMAP'
                 . '};'
-                . "var map = new google.maps.Map(document.getElementById(\"{$block['id']}\"), mapOptions);"
+                . "var map = new google.maps.Map(document.getElementById(id), mapOptions);"
                 . 'var marker = new google.maps.Marker({'
                     . 'position: myLatlng,'
                     . 'map: map,'
@@ -54,10 +57,17 @@ function ciniki_wng_generators_googlemap(&$ciniki, $tnid, $request, $block) {
                     . '});'
             . '};'
             . "function loadMap{$block['sid']}() {"
-                . 'var script = document.createElement("script");'
-                . 'script.type = "text/javascript";'
-                . 'script.src = "' . ($request['ssl']=='yes'?'https':'http') . '://maps.googleapis.com/maps/api/js?key=' . $ciniki['config']['ciniki.web']['google.maps.api.key'] . "&sensor=false&callback=gmap_initialize{$block['sid']}\";"
-                . 'document.body.appendChild(script);'
+                . 'if(C.gE("googlemap")==null){'
+                    . 'var script = document.createElement("script");'
+                    . 'script.setAttribute("id", "googlemap");'
+                    . 'script.type = "text/javascript";'
+                    . 'script.src = "' . ($request['ssl']=='yes'?'https':'http') . '://maps.googleapis.com/maps/api/js?key=' . $ciniki['config']['ciniki.web']['google.maps.api.key'] . "&sensor=false&callback=gmap_initialize{$block['sid']}\";"
+                    . 'document.body.appendChild(script);'
+                . '}else{'
+                    . "setTimeout(function(){"
+                        . "gmap_show('{$block['id']}',{$block['latitude']},{$block['longitude']});"
+                    . "}, 1000);"
+                . '}'
             . '};'
             . "addEventListener('load', (event) => {loadMap{$block['sid']}();});"
             . "";
