7d9579df2a3c6ee45647403232055b7f9fd5cf2a
Andrew Rivett <veggiefrog@gmail.com>
2023-04-09 21:40:03 -0400

    Added support for youtube shorts and vimeo video embeds

--- private/videoProcess.php
+++ private/videoProcess.php
@@ -21,10 +21,17 @@ function ciniki_wng_videoProcess($ciniki, $tnid, $request, $video_url) {
         $youtube_id = $m[1];
     } elseif( preg_match("/youtube.*\/embed\/([^\/\&]+)/", $video_url, $m) ) {
         $youtube_id = $m[1];
+    } elseif( preg_match("/youtube.*\/shorts\/([^\/\&]+)/", $video_url, $m) ) {
+        $youtube_id = $m[1];
+    } elseif( preg_match("/vimeo.*\/([^\/\&]+)/", $video_url, $m) ) {
+        $vimeo_id = $m[1];
     }
 
     if( isset($youtube_id) ) {
         $content = "<div class='video'><iframe src='https://www.youtube.com/embed/{$youtube_id}' title='YouTube Video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe></div>";
+    }
+    elseif( isset($vimeo_id) ) {
+        $content = "<div class='video'><iframe src='https://player.vimeo.com/video/{$vimeo_id}' allow='autoplay; fullscreen; picture-in-picture' allowfullscreen></iframe></div>";
     }
 
     return array('stat'=>'ok', 'content'=>$content);
