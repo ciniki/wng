45d926aa90e5e3a4af850886a054f0b0e2a7914f
Andrew Rivett <veggiefrog@gmail.com>
2023-09-21 11:23:11 -0400

    Removed timestamp from site.css and site.js

--- private/pageHeaderGenerate.php
+++ private/pageHeaderGenerate.php
@@ -92,8 +92,10 @@ function ciniki_wng_pageHeaderGenerate(&$ciniki, $tnid, $request) {
     //
     // Add CSS and javascript
     //
-    $content .= "<script src='" . $request['site']['cache_url'] . "/theme/site.js?ts=" . filemtime($request['site']['cache_dir'] . '/theme/site.js') . "'></script>\n";
-    $content .= "<link rel='stylesheet' type='text/css' media='all' href='" . $request['site']['cache_url'] . "/theme/site.css?ts=" . filemtime($request['site']['cache_dir'] . '/theme/site.css') . "'/>\n";
+//    $content .= "<script src='" . $request['site']['cache_url'] . "/theme/site.js?ts=" . filemtime($request['site']['cache_dir'] . '/theme/site.js') . "'></script>\n";
+//    $content .= "<link rel='stylesheet' type='text/css' media='all' href='" . $request['site']['cache_url'] . "/theme/site.css?ts=" . filemtime($request['site']['cache_dir'] . '/theme/site.css') . "'/>\n";
+    $content .= "<script src='" . $request['site']['cache_url'] . "/theme/site.js'></script>\n";
+    $content .= "<link rel='stylesheet' type='text/css' media='all' href='" . $request['site']['cache_url'] . "/theme/site.css'/>\n";
 
     //
     // Check for head scripts
