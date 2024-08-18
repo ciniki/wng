eae97a5b704b0effe89572fd377263b41a13497e
Andrew Rivett <veggiefrog@gmail.com>
2024-08-18 15:45:25 -0400

    Removed debug code

--- assets/block-livesearch.js
+++ assets/block-livesearch.js
@@ -11,7 +11,6 @@ C.livesearch = {
         var e=C.gE('livesearch-'+id);
         this.searches[id].args.search_string = e.value;
         C.getBg(this.searches[id].url,this.searches[id].args,function(rsp) {
-            console.log(rsp);
             if( rsp.content != null ) {
                 var r=C.gE('livesearch-results-'+id);
                 r.innerHTML = rsp.content;
