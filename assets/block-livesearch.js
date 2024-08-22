6a4c1d6b66b18591eaef40d83342de4cd7f42d94
Andrew Rivett <veggiefrog@gmail.com>
2024-08-21 22:52:25 -0400

    Fixed up livesearch to fix bugs with overlap results

--- assets/block-livesearch.js
+++ assets/block-livesearch.js
@@ -4,14 +4,18 @@ C.livesearch = {
         this.searches[id] = {
             'url':url,
             'args':args,
+            'curnum':0,
+            'disnum':0,
             };
         this.update(id);
     },
     update: function(id) {
         var e=C.gE('livesearch-'+id);
+        var num=++this.searches[id].curnum;
         this.searches[id].args.search_string = e.value;
         C.getBg(this.searches[id].url,this.searches[id].args,function(rsp) {
-            if( rsp.content != null ) {
+            if( rsp.content != null && num > C.livesearch.searches[id].disnum ) {
+                C.livesearch.searches[id].disnum = num;
                 var r=C.gE('livesearch-results-'+id);
                 r.innerHTML = rsp.content;
             }
