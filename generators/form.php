0cc59eea9feb547a1f4c779e6e310d940510b01e
Andrew Rivett <veggiefrog@gmail.com>
2025-02-14 13:28:20 -0500

    Fixed bug with no content process on form field descriptions

--- generators/form.php
+++ generators/form.php
@@ -788,9 +788,15 @@ function ciniki_wng_generators_form(&$ciniki, $tnid, $request, $block) {
             }
             $field_description = '';
             if( isset($field['description']) && $field['description'] != '' ) {
-                $field_description = "<div class='field-description'>"
-                    . $field['description']
-                    . "</div>";
+                $rc = ciniki_wng_contentProcess($ciniki, $tnid, $request, $field['description']);
+                if( $rc['stat'] != 'ok' ) {
+                    return $rc;
+                }
+                if( isset($rc['content']) && $rc['content'] != '' ) {
+                    $field_description = "<div class='field-description'>"
+                        . $rc['content']
+                        . "</div>";
+                }
                 $class .= ' description-included';
                 if( isset($field['class']) && $field['class'] != '' ) {
                     $field['class'] .= ' description-included';
