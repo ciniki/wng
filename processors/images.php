5307cecc4877dfafbd6c0b46f7e49b8c6247b34c
Andrew Rivett <veggiefrog@gmail.com>
2025-04-17 21:41:24 -0400

    Increased limit to 200 for website - images type

--- processors/images.php
+++ processors/images.php
@@ -23,7 +23,7 @@ function ciniki_wng_processors_images(&$ciniki, $tnid, &$request, $section) {
     $items = array();
     $requested_image = 0;
     $cur_item_num = 1;
-    for($i = 1; $i <= 100; $i++) {
+    for($i = 1; $i <= 200; $i++) {
         if( isset($s["image-{$i}"]) && $s["image-{$i}"] > 0 && is_numeric($s["image-{$i}"]) ) {
             $caption = '';
             if( isset($s["title-{$i}"]) && $s["title-{$i}"] != '' ) {
