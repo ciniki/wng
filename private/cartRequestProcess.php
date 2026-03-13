e3483d1cb0b49034ed6f43fefe7f9e271c12658e
Andrew Rivett <veggiefrog@gmail.com>
2026-03-12 23:18:31 -0400

    Changed how blocked are handled

--- private/cartRequestProcess.php
+++ private/cartRequestProcess.php
@@ -952,7 +952,7 @@ function ciniki_wng_cartRequestProcess(&$ciniki, $tnid, &$request) {
                     if( $rc['stat'] == 'updated' ) {
                         $updated = $rc['msg'];
                     } elseif( $rc['stat'] == 'blocked' ) {
-                        $blocked .= $rc['err']['msg'];
+                        $blocked .= $rc['blocked'];
                     } elseif( $rc['stat'] != 'ok' ) {
                         return array('stat'=>'fail', 'err'=>array('code'=>'ciniki.wng.119', 'msg'=>'Unable to confirm availability', 'err'=>$rc['err']));
                     }
